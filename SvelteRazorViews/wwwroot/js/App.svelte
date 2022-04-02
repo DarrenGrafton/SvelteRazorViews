1374296f8c7104178ce68349c0e41c15ee66ef29
Darren Grafton <darrenmgrafton@gmail.com>
2022-04-01 20:10:02 -0500

    Add project files.

---
+++ SvelteRazorViews/wwwroot/js/App.svelte
@@ -0,0 +1,19 @@
+<svelte:options tag="svelte-app" />
+<script>
+    export let name;
+    export let id;
+</script>
+
+<main>
+    <h1 id="{id}">Hello {name}!</h1>
+	<p>how are you doing?</p>
+	<p>Just fine....</p>
+</main>
+
+<style>
+    h1 {
+        font-size: 5em;
+		color: green;
+		margin-bottom: 0;
+    }
+</style>
